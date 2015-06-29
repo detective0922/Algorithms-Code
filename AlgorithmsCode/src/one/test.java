a1be1c950702d9ef3ad6c3ade7af1ad0b2338b1c
detective0922 <detective0922@hotmail.com>
2015-06-29 15:40:02 +0800

    ok

--- AlgorithmsCode/src/one/test.java
+++ AlgorithmsCode/src/one/test.java
@@ -9,10 +9,10 @@ public class test {
 	
 	public static int mystery(int a, int b){
 		if (b == 0)
-			return 1;
+			return 0;
 		if (b % 2 == 0)
-			return mystery(a * a, b / 2);
-		return mystery(a * a, b / 2) * a;
+			return mystery(a + a, b / 2);
+		return mystery(a + a, b / 2) + a;
 	}
 
 }
