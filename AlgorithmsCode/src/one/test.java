98b3a18ea42f991827c55b0c4d001e4c20f63e62
detective0922 <detective0922@hotmail.com>
2015-06-29 15:30:23 +0800

    ok

--- AlgorithmsCode/src/one/test.java
+++ AlgorithmsCode/src/one/test.java
@@ -1,9 +1,18 @@
 package one;
 
 public class test {
-
-	public test() {
-		// TODO Auto-generated constructor stub
+	
+	public static void main(String args[]){
+		System.out.println(mystery(2,25));
+		System.out.println(mystery(3,11));
+	}
+	
+	public static int mystery(int a, int b){
+		if (b == 0)
+			return 1;
+		if (b % 2 == 0)
+			return mystery(a * a, b / 2);
+		return mystery(a * a, b / 2) * a;
 	}
 
 }
