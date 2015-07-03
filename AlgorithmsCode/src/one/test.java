9ecfd81a23a4fdd9ea03525151c6ea9e2c7baf38
detective0922 <detective0922@hotmail.com>
2015-07-03 09:33:07 +0800

    ok

--- AlgorithmsCode/src/one/test.java
+++ AlgorithmsCode/src/one/test.java
@@ -9,14 +9,16 @@ public class test {
 		
 		long time1 = System.nanoTime();
 		//String string = Long.toBinaryString(Long.MAX_VALUE);
-		String string = toBinaryString(Long.MAX_VALUE);
+		//String string = toBinaryString(Long.MAX_VALUE);
+		int a = lg(1023);
 		long time2 = System.nanoTime();
 		System.out.println(time1);
 		System.out.println(time2);
 		System.out.println(time2 - time1);
-		System.out.println(string);
+		System.out.println(a);
 	}
 	
+	//1.1.18
 	public static int mystery(int a, int b){
 		if (b == 0)
 			return 0;
@@ -27,6 +29,7 @@ public class test {
 		
 	}
 	
+	//1.1.9
 	public static String toBinaryString(long val) {
 		char[] buf = new char[64];
 		int pos = 63;
@@ -41,6 +44,34 @@ public class test {
 		}
 		return new String(buf, pos+1, 64-(pos+1));
 	}
+	
+	//1.1.14
+	public static int lg(int n){
+		int i = 0;
+		int m = 2;
+		while (m <= n) {
+			m = m*2;
+			i++;
+		}
+		return i;
+	}
+	
+	//1.1.15
+	public static int[] histogram(int[] a, int m){
+		int[] array = new int[m];
+		for (int i = 0; i < m; i++) {
+			int count = 0;
+			for (int j = 0; i < a.length; i++) {
+				if (a[j] == i + 1)
+					count++;
+			}
+			array[i] = count;
+		}
+		
+		return array;
+		
+	}
+	
 	
 	final static char[] digits = {
 		'0' , '1' , '2' , '3' , '4' , '5' ,
