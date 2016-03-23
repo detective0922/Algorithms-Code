6c9e58eec0d6ed0c554aae4769f06bd1436f09ad
detective0922 <detective0922@hotmail.com>
2016-03-23 17:19:50 +0800

    add comment

--- AlgorithmsCode/src/Part1_Fundamentals/Chapter1_1_ProgrammingModel/Test1_1_16.java
+++ AlgorithmsCode/src/Part1_Fundamentals/Chapter1_1_ProgrammingModel/Test1_1_16.java
@@ -6,7 +6,7 @@ public class Test1_1_16 {
 
 	public static void main(String args[]) {
 		StdOut.println(exR1(6));
-		//print 311361142246
+		//output: 311361142246
 	}
 
 	public static String exR1(int n) {
