3074eda3bd7ba2d19980658d27e5d4c0cf7fc04c
detective0922 <detective0922@hotmail.com>
2016-03-24 11:18:01 +0800

    1-1-22

---
+++ AlgorithmsCode/src/Part1_Fundamentals/Chapter1_1_ProgrammingModel/Test1_1_22.java
@@ -0,0 +1,47 @@
+package Part1_Fundamentals.Chapter1_1_ProgrammingModel;
+
+import java.io.File;
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Test1_1_22 {
+
+	public static void main(String args[]) {
+		File wFile = new File("algs4-data//tinyW.txt");
+		File tFile = new File("algs4-data//tinyT.txt");
+		int[] whitelist = new In(wFile).readAllInts();
+		int[] inList = new In(tFile).readAllInts();
+		Arrays.sort(whitelist);
+		for (int i = 0; i < inList.length; i++) {
+			int key = inList[i];
+			if (rank1(key, whitelist) < 0) {
+				StdOut.println("key:" + key);
+			} else {
+				StdOut.println("key in white list");
+			}
+		}
+	}
+
+	public static int rank1(int key, int[] a) {
+		return rank(key, a, 0, a.length - 1, 0);
+	}
+	
+	public static int rank(int key, int[] a, int lo, int hi, int depth) {
+		if (lo > hi)
+			return -1;
+		int mid = lo + (hi - lo) / 2;		
+		String outStr = "lo: " + lo + ",hi: " + hi;
+		String format = "%" + (depth + outStr.length()) + "s\n";
+		StdOut.printf(format, outStr);
+		depth++;
+		if (key < a[mid])
+			return rank(key, a, lo, mid - 1, depth);
+		else if (key > a[mid])
+			return rank(key, a, mid + 1, hi, depth);
+		else
+			return mid;
+	}
+
+}
