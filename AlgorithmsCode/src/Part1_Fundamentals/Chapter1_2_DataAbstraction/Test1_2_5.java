3cf602bfd80875417ab53df5e3fab73fead931c5
detective0922 <detective0922@hotmail.com>
2016-03-27 18:26:55 +0800

    1-2-5

---
+++ AlgorithmsCode/src/Part1_Fundamentals/Chapter1_2_DataAbstraction/Test1_2_5.java
@@ -0,0 +1,17 @@
+package Part1_Fundamentals.Chapter1_2_DataAbstraction;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Test1_2_5 {
+	
+	public static void main(String args[]) {
+		String s = "Hello World";
+		s.toUpperCase();
+		s.substring(6, 11);
+		StdOut.println(s);
+		//output:
+		//Hello World
+
+	}
+
+}
