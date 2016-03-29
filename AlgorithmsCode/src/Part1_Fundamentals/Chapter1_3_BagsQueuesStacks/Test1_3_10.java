f5ba234c63838853a7c0dea6748b83f712eba008
detective0922 <detective0922@hotmail.com>
2016-03-29 14:04:01 +0800

    1-3-10

---
+++ AlgorithmsCode/src/Part1_Fundamentals/Chapter1_3_BagsQueuesStacks/Test1_3_10.java
@@ -0,0 +1,63 @@
+package Part1_Fundamentals.Chapter1_3_BagsQueuesStacks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Test1_3_10 {
+	
+	public static void main(String[] args) {
+	
+		Map<String, Integer> priorityMap = new HashMap<String, Integer>();
+		priorityMap.put("+", 0);
+		priorityMap.put("-", 0);
+		priorityMap.put("*", 1);
+		priorityMap.put("/", 1);
+		priorityMap.put("(", -1);
+		
+		String inputStr = "( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )";
+		String[] strs = inputStr.split(" ");
+		StdOut.println(inputStr);
+		
+        Stack<String> ops  = new Stack<String>();
+        StringBuffer newExpression = new StringBuffer();
+
+		for (String s : strs) {
+			
+			if (s.equals("(")) {
+				ops.push(s);
+			} else if (s.equals("+") || s.equals("-")||s.equals("*")||s.equals("/")) {
+				String opInStack = ops.peek();
+				int priority = priorityMap.get(s);
+				int priorityInStack = priorityMap.get(opInStack);
+				if (priority > priorityInStack) {
+					ops.push(s);
+				}
+				while (!ops.isEmpty() && priority <= priorityInStack) {
+					newExpression.append(ops.pop());
+					opInStack = ops.peek();
+					priorityInStack = priorityMap.get(opInStack);
+				}
+			} else if (s.equals(")")) {
+				while (!ops.isEmpty() && !ops.peek().equals("(")) {
+					newExpression.append(ops.pop());
+				}
+				if (ops.peek().equals("("))
+					ops.pop();
+			} else {
+				newExpression.append(s);
+			}
+        }
+		while (!ops.isEmpty()) {
+			String op = ops.pop();
+			if (!op.equals("("))
+				newExpression.append(op);
+		}
+		StdOut.print(newExpression.toString());		
+		//output:
+		//( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) ) 
+		//12+34-56-**
+    }
+
+}
