ffb528a814027aff35f87930e3b75465e2682a91
detective0922 <detective0922@hotmail.com>
2016-04-07 10:10:00 +0800

    1-4-18

--- AlgorithmsCode/src/Part1_Fundamentals/Chapter1_4_AnalysisofAlgorithms/Test1_4_18.java
+++ AlgorithmsCode/src/Part1_Fundamentals/Chapter1_4_AnalysisofAlgorithms/Test1_4_18.java
@@ -9,9 +9,9 @@ import edu.princeton.cs.algs4.StdOut;
 public class Test1_4_18 {
 	
 	public static void main(String[] args) {
-		File tFile = new File("algs4-data//1Mints.txt");
-		int[] inList = new In(tFile).readAllInts();
-		//int[] inList = { -4, -3, -2, -1, 4, 3, 1, 2 };
+		//File tFile = new File("algs4-data//1Mints.txt");
+		//int[] inList = new In(tFile).readAllInts();
+		int[] inList = { -4, -3, -2, -1, 0, 1, -1, 3, -5};
 		int min = findLocalMin(inList);
 		if (min != -1) {			
 			if (min - 1 >= 0) {
@@ -25,6 +25,45 @@ public class Test1_4_18 {
 			StdOut.println("not found");
 		}
 	}
+	
+	/*public static int localMin(int[] a) {
+		int lo = 0;
+		int hi = a.length - 1;
+		return findLocalMin(a, lo, hi);
+	}
+	
+	public static int findLocalMin(int[] a, int lo, int hi) {
+		int N = a.length;
+		if (N == 1)
+			return 0;
+		if (a[0] < a[1]) {
+			return 0;
+		}
+		if (a[N - 1] > a[N - 2]) {
+			return N - 1;
+		}
+		if (lo > hi)
+			return -1;
+		int mid = lo + (hi - lo) / 2;
+		if (mid == N - 1 || mid == 0) {
+			return -1;
+		}
+		if (a[mid] < a[mid - 1] && a[mid] < a[mid + 1]) {
+			return mid;
+		}
+		int left = -1;
+		int right = -1;
+		if (a[mid] > a[mid - 1]) {
+			left = findLocalMin(a, lo, mid - 1);
+		}
+		if (a[mid] > a[mid + 1]) {
+			right = findLocalMin(a, mid + 1, hi);
+		}
+		if(left != -1){
+			return left;
+		}
+		return right;
+	}*/
 	
 	public static int findLocalMin(int[] a) {
 		int N = a.length;
