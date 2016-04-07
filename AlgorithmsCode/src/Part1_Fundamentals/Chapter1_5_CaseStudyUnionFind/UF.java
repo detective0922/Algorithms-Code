b121ef459767e4f641cc1c921ceef9871475732f
detective0922 <detective0922@hotmail.com>
2016-04-07 14:45:54 +0800

    UF

---
+++ AlgorithmsCode/src/Part1_Fundamentals/Chapter1_5_CaseStudyUnionFind/UF.java
@@ -0,0 +1,55 @@
+package Part1_Fundamentals.Chapter1_5_CaseStudyUnionFind;
+
+import java.io.File;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class UF {
+	
+	public static void main(String[] args) {
+		File tFile = new File("algs4-data//tinyUF.txt");
+		In in = new In(tFile);
+		int N = in.readInt();
+		UF uf = new UF(N);
+		while (in.isEmpty()) {
+			int p = in.readInt();
+			int q = in.readInt();
+			if (uf.connected(p, q))
+				continue;
+			uf.union(p, q);
+			StdOut.println(p + ", " + q);
+		}
+		
+		StdOut.println(uf.count + "components");
+
+	}
+	
+	private int[] id;
+	private int count;
+	
+	public UF(int N) {
+		count = N;
+		id = new int[N];
+		for (int i = 0; i < N; i++) {
+			id[i] = i;
+		}
+	}
+	
+	public void union(int p, int q){
+		id[p] = id[q];
+	}
+	
+	public int find(int p){
+		return id[p];
+	}
+	
+	public boolean connected(int p, int q) {
+		return find(p) == find(q);
+	}
+	
+	public int count(){
+		return count;
+	}
+
+}
