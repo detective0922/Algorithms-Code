b43936a0e6f63701368ce7b14f4d4e35096a6f3a
detective0922 <detective0922@hotmail.com>
2016-04-15 16:08:03 +0800

    Quicksort

--- AlgorithmsCode/src/Part2_Sorting/Chapter2_3_Quicksort/Quicksort.java
+++ AlgorithmsCode/src/Part2_Sorting/Chapter2_3_Quicksort/Quicksort.java
@@ -17,33 +17,61 @@ public class Quicksort {
 	}
 	
 	public static void sort(Comparable[] a){
-		StdRandom.shuffle(a);
-		sort(a);
+		//StdRandom.shuffle(a);
+		sort(a, 0, a.length - 1);
 	}
 	
 	private static void sort(Comparable[] a, int lo, int hi) {
 		if(hi<=lo)
 			return;
 		int j = partition(a, lo, hi);
+		//int j = partitionOnBook(a, lo, hi);
+		StdOut.println(j + "," + a[j]);
 		sort(a, lo, j - 1);
 		sort(a, j + 1, hi);
 	}
 	
 	private static int partition(Comparable[] a, int lo, int hi){
-		int i = lo;
+		int i = lo + 1;
 		int j = hi;
 		Comparable v = a[lo];
-		while (i < j) {
-			if (less(a[i], v)) {
-				i++;
-			}
-			if (less(v, a[j])) {
-				j--;
-			}
+		while (i <= j) {
 			if (less(v, a[i]) && less(a[j], v)) {
 				exch(a, i, j);
+				i++;
+				j--;
 			}
+			if (!less(v, a[i])) {
+				i++;
+			}
+			if (!less(a[j], v)) {
+				j--;
+			}
+
 		}
+		exch(a, lo, j);
+		return j;
+	}
+	
+	private static int partitionOnBook(Comparable[] a, int lo, int hi){
+
+		int i = lo;
+		int j = hi + 1;
+		Comparable v = a[lo];
+		while (true) {
+			while (less(a[++i], v)) {
+				if (i == hi)
+					break;
+			}
+			while (less(v, a[--j])) {
+				if (j == lo)
+					break;
+			}
+			if (i >= j)
+				break;
+			exch(a, i, j);
+		}
+		exch(a, lo, j);
 		return j;
 	}
 
