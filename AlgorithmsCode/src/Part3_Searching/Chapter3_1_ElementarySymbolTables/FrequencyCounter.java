708bd7990060d1e93b592dd1bb77ac42f97c5cfc
detective0922 <detective0922@hotmail.com>
2016-04-27 16:35:18 +0800

    SequentialSearchST

--- AlgorithmsCode/src/Part3_Searching/Chapter3_1_ElementarySymbolTables/FrequencyCounter.java
+++ AlgorithmsCode/src/Part3_Searching/Chapter3_1_ElementarySymbolTables/FrequencyCounter.java
@@ -1,30 +1,84 @@
 package Part3_Searching.Chapter3_1_ElementarySymbolTables;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Transaction;
 
 public class FrequencyCounter {
 	public static void main(String[] args) {
-		int minlen = Integer.parseInt(args[0]);
+		int minlen = 6;
 		SequentialSearchST<String, Integer> ssst = new SequentialSearchST<String, Integer>();
-		while(!StdIn.isEmpty()){
-			String word = StdIn.readString();
-			if(word.length()<minlen)continue;
-			if(!ssst.contains(word)) ssst.put(word,1);
-			else ssst.put(word,ssst.get(word)+1);
-		}
+		File tFile = new File("algs4-data//tinyST.txt");
+		String[] inList = new In(tFile).readAllLines();
 		
+		for (int i = 0; i < inList.length; i++) {
+			String word = inList[i];
+			if (word.length() < minlen)
+				continue;
+			if (!ssst.contains(word))
+				ssst.put(word, 1);
+			else
+				ssst.put(word, ssst.get(word) + 1);
+		}
+
 		String max = " ";
-		ssst.put(max,0);
-		for(String word: ssst.keys()){
-			if(ssst.get(word)>ssst.get(max))
+		ssst.put(max, 0);
+		for (String word : ssst.keys()) {
+			if (ssst.get(word) > ssst.get(max))
 				max = word;
 		}
-		StdOut.println(max+", "+ssst.get(max));
+		StdOut.println(max + ", " + ssst.get(max));
 	}
 }
 
 class SequentialSearchST<Key, Value>{
 	
+	private Node first;
+	
+	private class Node {
+		Key key;
+		Value value;
+		Node next;
+
+		public Node(Key key, Value val, Node next) {
+			this.key = key;
+			this.value = val;
+			this.next = next;
+		}
+	}
+	
+	public Value get(Key key) {
+		Node node = first;
+		while (!key.equals(node.key)) {
+			node = node.next;
+		}
+		return node.value;
+	}
+	
+	public void put(Key key, Value value) {
+		Node node = new Node(key, value, null);
+		Node current = first;
+		while (current.next != null) {
+			current = current.next;
+		}
+		current.next = node;
+	}
+	
+	public boolean contains(Key key) {
+		return get(key) != null;
+	}
+	
+	public Iterable<Key> keys(){
+		List<Key> keys = new ArrayList<Key>();
+		for (Node node = first; node.next != null; node = node.next) {
+			keys.add(node.key);
+		}
+		return keys;		
+	}
 }
 
