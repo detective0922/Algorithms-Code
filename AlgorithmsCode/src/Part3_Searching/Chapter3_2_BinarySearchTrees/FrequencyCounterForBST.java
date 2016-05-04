7f0d4d467db04bd4dd5322a0d8b3cc87d4cbe429
detective0922 <detective0922@hotmail.com>
2016-05-04 16:48:22 +0800

    BST

--- AlgorithmsCode/src/Part3_Searching/Chapter3_2_BinarySearchTrees/FrequencyCounterForBST.java
+++ AlgorithmsCode/src/Part3_Searching/Chapter3_2_BinarySearchTrees/FrequencyCounterForBST.java
@@ -94,7 +94,7 @@ class BST<Key extends Comparable<Key>, Value>{
 			return node.N;
 	}
 	
-	public Value get(Key key) {
+	/*public Value get(Key key) {
 		if(isEmpty())
 			return null;
 		Node node = root;
@@ -109,9 +109,25 @@ class BST<Key extends Comparable<Key>, Value>{
 			}
 		}
 		return node.value;
+	}*/
+	
+	public Value get(Key key) {
+		return get(root, key);
 	}
 	
-	public void put(Key key, Value value) {
+	private Value get(Node x, Key key) {
+		if (x == null)
+			return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0)
+			return get(x.left, key);
+		else if (cmp > 0)
+			return get(x.right, key);
+		else
+			return x.value;
+	}
+	
+	/*public void put(Key key, Value value) {
 		if (isEmpty()) {
 			root = new Node(key, value, 1);
 			return;
@@ -132,6 +148,24 @@ class BST<Key extends Comparable<Key>, Value>{
 		}
 		
 		node = new Node(key, value, node.N + 1);
+	}*/
+	
+	public void put(Key key, Value val){
+		root = put(root, key, val);
+	}
+	
+	private Node put(Node x, Key key, Value val){
+		if (x == null)
+			return new Node(key, val, 1);
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0)
+			return x.left = put(x.left, key, val);
+		else if (cmp > 0)
+			return x.right = put(x.right, key, val);
+		else
+			x.value = val;
+		
+		return x;
 	}
 	
 	public boolean isEmpty(){
