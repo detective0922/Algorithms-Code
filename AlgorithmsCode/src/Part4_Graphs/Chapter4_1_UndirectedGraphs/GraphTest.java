337304746067107c89a4c777854b9e1df3f53162
detective0922 <detective0922@hotmail.com>
2016-06-22 15:17:24 +0800

    Graph

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_1_UndirectedGraphs/GraphTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_1_UndirectedGraphs/GraphTest.java
@@ -72,7 +72,7 @@ class Graph{
 	
 	public int degree(Graph G, int v){
 		int degree = 0;
-		for(int w:G.adj[v]){
+		for (int w : G.adj[v]) {
 			degree++;
 		}
 		return degree;
@@ -80,8 +80,8 @@ class Graph{
 	
 	public int maxDegree(Graph G){
 		int max = 0;
-		for (int w = 0; w < G.V(); w++) {
-			int degree = degree(G, w);
+		for (int v = 0; v < G.V(); v++) {
+			int degree = degree(G, v);
 			if (degree > max) {
 				max = degree;
 			}
@@ -94,10 +94,26 @@ class Graph{
 	}
 	
 	public int numberOfSelfLoops(Graph G){
-		
+		int count = 0;
+		for(int v = 0;v<G.V();v++){
+			for(int w: G.adj[v]){
+				if(v == w){
+					count++;
+				}
+			}
+		}
+		return count / 2;
 	}
 	
-	public String toString(){
-		
+	public String toString() {
+		String s = V + " vertices, " + E + " edges\n";
+		for (int v = 0; v < V; v++) {
+			s += v + ": ";
+			for (int w : adj[v]) {
+				s += w + " ";
+			}
+			s += "\n";
+		}
+		return s;
 	}
 }
