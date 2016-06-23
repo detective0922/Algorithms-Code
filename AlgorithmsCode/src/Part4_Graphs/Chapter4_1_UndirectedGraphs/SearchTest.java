697e1bee5ad355040d293a2b1a815a512b94b6dd
detective0922 <detective0922@hotmail.com>
2016-06-23 10:07:28 +0800

    DFS

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_1_UndirectedGraphs/SearchTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_1_UndirectedGraphs/SearchTest.java
@@ -1,13 +1,18 @@
 package Part4_Graphs.Chapter4_1_UndirectedGraphs;
 
+import java.io.File;
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class SearchTest {
 	public static void main(String[] args){
-		Graph g = new Graph(new In(args[0]));
-		int s = Integer.parseInt(args[1]);
-		Search search = new Search(g, s);
+		
+		File tFile = new File("algs4-data//tinyG.txt");
+		In in = new In(tFile);
+		Graph g = new Graph(in);
+		int s = 0;
+		DepthFirstSearch search = new DepthFirstSearch(g, s);
 		
 		for(int v = 0; v<g.V();v++){
 			if(search.marked(v)){
@@ -18,9 +23,28 @@ public class SearchTest {
 		
 		if(search.count()!=g.V()){
 			StdOut.println("Not connected");
-		}
-		
-		
+		}			
+	}
+}
+
+class DepthFirstSearch {
+	private boolean[] marked;
+	private int count;
+	
+	public DepthFirstSearch(Graph g, int s) {
+		marked = new boolean[g.V()];
+		count = 0;
+		dfs(g, s);
+	}
+	
+	
+	
+	public boolean marked(int v) {
+		return marked[v];
+	}
+	
+	public int count(){
+		return count;
 	}
 
 }
