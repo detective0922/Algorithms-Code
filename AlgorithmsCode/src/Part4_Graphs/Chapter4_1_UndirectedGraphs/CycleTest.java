edfa6d26ba090a05bab5feaee06f385322ba7771
detective0922 <detective0922@hotmail.com>
2016-06-27 11:16:44 +0800

    Cycle

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_1_UndirectedGraphs/CycleTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_1_UndirectedGraphs/CycleTest.java
@@ -1,5 +1,58 @@
 package Part4_Graphs.Chapter4_1_UndirectedGraphs;
 
+import java.io.File;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class CycleTest {
 
+	public static void main(String[] args) {
+
+		File tFile = new File("algs4-data//tinyG.txt");
+		In in = new In(tFile);
+		Graph g = new Graph(in);
+		Cycle c = new Cycle(g);
+
+		StdOut.println(c.hasCycle());
+	}
+}
+
+class Cycle{
+	private boolean[] marked;
+	private boolean hasCycle;
+	
+	public Cycle(Graph g) {
+		marked = new boolean[g.V()];
+		hasCycle = false;
+		for (int v = 0; v < g.V(); v++) {
+			if (!marked[v]) {
+				dfs(g, v, v);
+			}
+		}
+	}
+	
+	private void dfs(Graph g, int currentV, int lastV) {
+
+		marked[currentV] = true;
+		Iterable<Integer> sAdj = g.adj(currentV);
+		for (int w : sAdj) {
+			if (!marked[w]) {
+				dfs(g, w, lastV);
+			} else if( w != lastV){
+				hasCycle = true;
+			}
+		}
+	}
+	
+	public boolean marked(int v) {
+		return marked[v];
+	}
+	
+	public boolean hasCycle(){
+		return hasCycle;
+	}
+	
+	
 }
