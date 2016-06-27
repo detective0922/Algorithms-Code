ac7e4cf5a07f50aa845c98f72eed4602d74ad1bb
detective0922 <detective0922@hotmail.com>
2016-06-27 19:45:37 +0800

    SG

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_1_UndirectedGraphs/SymbolGraphTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_1_UndirectedGraphs/SymbolGraphTest.java
@@ -1,10 +1,29 @@
 package Part4_Graphs.Chapter4_1_UndirectedGraphs;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class SymbolGraphTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String filename = args[0];
+		String delimiter = args[1];
+		SymbolGraph sg = new SymbolGraph(filename, delimiter);
+		Graph G = sg.G();
+
+		String source = "";
+		if (sg.contains(source)) {
+			int s = sg.index(source);
+			for (int v : G.adj(s)) {
+				StdOut.println("   " + sg.name(v));
+			}
+		} else {
+			StdOut.println("input not contain '" + source + "'");
+		}
 
 	}
+}
 
+class SymbolGraph{
+	
 }
