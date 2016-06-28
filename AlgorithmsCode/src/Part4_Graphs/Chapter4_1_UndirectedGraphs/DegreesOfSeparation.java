4b28ff469dbec960a53d16ee4c1a15d4475719e1
detective0922 <detective0922@hotmail.com>
2016-06-28 11:37:24 +0800

    degree

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_1_UndirectedGraphs/DegreesOfSeparation.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_1_UndirectedGraphs/DegreesOfSeparation.java
@@ -6,27 +6,39 @@ public class DegreesOfSeparation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*String filename = "algs4-data//routes.txt";
+		String delimiter = " ";*/
 		String filename = "algs4-data//movies.txt";
 		String delimiter = "/";
 		SymbolGraph sg = new SymbolGraph(filename, delimiter);
 		Graph G = sg.G();
 		
+		//String source = "JFK";
 		String source = "Bacon, Kevin";
-		StdOut.println(source + ":");
+		BreadthFirstPath bfs = null;		
 		if (sg.contains(source)) {
 			int s = sg.index(source);
-			BreadthFirstPath bfs = new BreadthFirstPath(G, s);
-			if (bfs.hasPathTo(s)) {
-				for (int v : bfs.pathTo(s)) {
+			bfs = new BreadthFirstPath(G, s);
+		} else {
+			StdOut.println("input not contain '" + source + "'");
+		}	
+		
+		//String sink = "LAS";
+		//String sink = "DFW";
+		String sink = "Kidman, Nicole";
+		StdOut.println(sink + ":");
+		if (sg.contains(sink)) {
+			int t = sg.index(sink);
+			if (bfs.hasPathTo(t)) {
+				for (int v : bfs.pathTo(t)) {
 					StdOut.println("   " + sg.name(v));
 				}
 			} else {
 				StdOut.println("no path to " + source);
 			}
 		} else {
-			StdOut.println("input not contain '" + source + "'");
-		}
-		
+			StdOut.println("input not contain '" + sink + "'");
+		}	
 		
 	}
 
