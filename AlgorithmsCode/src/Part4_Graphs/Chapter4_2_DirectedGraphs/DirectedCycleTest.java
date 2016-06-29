8413e5508ee2e16329845ff643ea897dbc854aa4
detective0922 <detective0922@hotmail.com>
2016-06-29 15:28:17 +0800

    dcycle

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_2_DirectedGraphs/DirectedCycleTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_2_DirectedGraphs/DirectedCycleTest.java
@@ -3,27 +3,37 @@ package Part4_Graphs.Chapter4_2_DirectedGraphs;
 import java.io.File;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
 public class DirectedCycleTest {
 
 	public static void main(String[] args) {
 
-		File tFile = new File("algs4-data//tinyG.txt");
+		File tFile = new File("algs4-data//tinyDG.txt");
 		In in = new In(tFile);
 		Digraph g = new Digraph(in);
 		DirectedCycle c = new DirectedCycle(g);
 
 		StdOut.println(c.hasCycle());
+		if (c.hasCycle()) {
+            for (int v : c.cycle()) {
+                StdOut.print(v + " ");
+            }
+            StdOut.println();
+        }
 	}
 }
 
 class DirectedCycle{
 	private boolean[] marked;
+	private int[] edgeTo;
+	private Stack<Integer> cycle;
 	private boolean hasCycle;
 	
 	public DirectedCycle(Digraph g) {
 		marked = new boolean[g.V()];
+		edgeTo = new int[g.V()];
 		hasCycle = false;
 		for (int v = 0; v < g.V(); v++) {
 			if (!marked[v]) {
@@ -38,9 +48,16 @@ class DirectedCycle{
 		Iterable<Integer> sAdj = g.adj(currentV);
 		for (int w : sAdj) {
 			if (!marked[w]) {
-				dfs(g, w, lastV);
+				edgeTo[w] = currentV;
+				dfs(g, w, currentV);
 			} else if( w != lastV){
 				hasCycle = true;
+				cycle = new Stack<Integer>();
+				for (int x = currentV; x != w; x = edgeTo[x]) {
+					cycle.push(x);
+				}
+				cycle.push(w);
+				cycle.push(currentV);
 			}
 		}
 	}
@@ -51,6 +68,10 @@ class DirectedCycle{
 	
 	public boolean hasCycle(){
 		return hasCycle;
+	}
+	
+	public Iterable<Integer> cycle() {
+		return cycle;
 	}
 	
 	
