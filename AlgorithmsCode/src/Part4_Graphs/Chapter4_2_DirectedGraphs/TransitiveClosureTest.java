5ebd798d44106b31791ffa8ff07a653f9bff0d1e
detective0922 <detective0922@hotmail.com>
2016-07-01 11:29:46 +0800

    tc

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_2_DirectedGraphs/TransitiveClosureTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_2_DirectedGraphs/TransitiveClosureTest.java
@@ -2,7 +2,6 @@ package Part4_Graphs.Chapter4_2_DirectedGraphs;
 
 import java.io.File;
 
-import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
@@ -26,9 +25,9 @@ public class TransitiveClosureTest {
         StdOut.println("--------------------------------------------");
 
         // print transitive closure
-        for (int v = 0; v < G.V(); v++) {
+        for (int v = 0; v < g.V(); v++) {
             StdOut.printf("%3d: ", v);
-            for (int w = 0; w < G.V(); w++) {
+            for (int w = 0; w < g.V(); w++) {
                 if (tc.reachable(v, w)) StdOut.printf("  T");
                 else                    StdOut.printf("   ");
             }
@@ -39,8 +38,12 @@ public class TransitiveClosureTest {
 
 class TransitiveClosure{
 	private DirectedDFS[] all;
+
 	public TransitiveClosure(Digraph g) {
-		
+		all = new DirectedDFS[g.V()];
+		for (int v = 0; v < g.V(); v++) {
+			all[v] = new DirectedDFS(g, v);
+		}
 	}
 	
 	public boolean reachable(int v, int w){
