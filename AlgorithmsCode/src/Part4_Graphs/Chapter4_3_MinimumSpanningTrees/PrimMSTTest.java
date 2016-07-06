f769b2534c86bdc113d83ca6db11d211c76be87c
detective0922 <detective0922@hotmail.com>
2016-07-06 09:41:47 +0800

    PrimMST

---
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_3_MinimumSpanningTrees/PrimMSTTest.java
@@ -0,0 +1,37 @@
+package Part4_Graphs.Chapter4_3_MinimumSpanningTrees;
+
+import java.io.File;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class PrimMSTTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File tFile = new File("algs4-data//tinyEWG.txt");		
+		In in = new In(tFile);
+        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+        PrimMST mst = new PrimMST(G);
+        for (Edge e : mst.edges()) {
+            StdOut.println(e);
+        }
+        StdOut.println(mst.weight());
+	}
+
+}
+
+class PrimMST{
+	public PrimMST(EdgeWeightedGraph g){
+		
+	}
+	
+	public Iterable<Edge> edges(){
+		return null;
+	}
+	
+	public double weight(){
+		return 0;
+	}
+	
+}
