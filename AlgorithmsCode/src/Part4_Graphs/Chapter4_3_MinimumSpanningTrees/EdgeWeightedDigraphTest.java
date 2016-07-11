007fd784c1ccdd2f8a815a936fb578c311689676
detective0922 <detective0922@hotmail.com>
2016-07-12 02:39:20 +0800

    dwd

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_3_MinimumSpanningTrees/EdgeWeightedDigraphTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_3_MinimumSpanningTrees/EdgeWeightedDigraphTest.java
@@ -1,5 +1,7 @@
 package Part4_Graphs.Chapter4_3_MinimumSpanningTrees;
 
+import edu.princeton.cs.algs4.Digraph;
+
 public class EdgeWeightedDigraphTest {
 
 	public static void main(String[] args) {
@@ -10,7 +12,7 @@ public class EdgeWeightedDigraphTest {
 }
 
 class EdgeWeightedDigraph{
-	public EdgeWeightedDigraph(){
+	public EdgeWeightedDigraph(Digraph g){
 		
 	}
 }
