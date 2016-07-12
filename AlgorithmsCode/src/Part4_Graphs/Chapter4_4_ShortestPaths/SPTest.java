69cc1b6c06c318eb1b2da1b63cf3be5617409713
detective0922 <detective0922@hotmail.com>
2016-07-12 13:53:01 +0800

    SP

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_4_ShortestPaths/SPTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_4_ShortestPaths/SPTest.java
@@ -3,6 +3,7 @@ package Part4_Graphs.Chapter4_4_ShortestPaths;
 import java.io.File;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
 public class SPTest {
@@ -18,29 +19,41 @@ public class SPTest {
 		
 		for (int v = 0; v < g.V(); v++) {
 			StdOut.print(s + " to " + v);
-			StdOut.printf(" (%4.2f): ", sp.distTo());
-			if (sp.hasPathTo()) {
-				for (DirectedEdge e : sp.pathTo()) {
+			StdOut.printf(" (%4.2f): ", sp.distTo(v));
+			if (sp.hasPathTo(v)) {
+				for (DirectedEdge e : sp.pathTo(v)) {
 					StdOut.print(e + " ");
 				}
 			}
 		}
-		
-
 	}
 
 }
 
 class SP{
-	public boolean hasPathTo() {
-		
+	
+	private double[] distTo;
+    private DirectedEdge[] edgeTo;
+	
+	public SP(EdgeWeightedDigraph g, int v) {
+
 	}
 	
-	public double distTo() {
-		
+	public boolean hasPathTo(int v) {
+		return distTo[v] < Double.POSITIVE_INFINITY;
 	}
 	
-	public Iterable<DirectedEdge> pathTo() {
-		
+	public double distTo(int v) {
+		return distTo[v];
+	}
+	
+	public Iterable<DirectedEdge> pathTo(int v) {
+		Stack<DirectedEdge> stack = new Stack<DirectedEdge>();
+		if (hasPathTo(v)) {
+			for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+				stack.push(e);
+			}
+		}
+		return stack;
 	}
 }
