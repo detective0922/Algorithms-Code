f538368184ac35bc2fc4a3ebb9ba8c466294e22f
detective0922 <detective0922@hotmail.com>
2016-07-12 10:14:05 +0800

    dwd

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_3_MinimumSpanningTrees/EdgeWeightedDigraphTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_3_MinimumSpanningTrees/EdgeWeightedDigraphTest.java
@@ -2,7 +2,7 @@ package Part4_Graphs.Chapter4_3_MinimumSpanningTrees;
 
 import java.io.File;
 
-import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
 
 public class EdgeWeightedDigraphTest {
@@ -18,7 +18,65 @@ public class EdgeWeightedDigraphTest {
 }
 
 class EdgeWeightedDigraph{
-	public EdgeWeightedDigraph(Digraph g){
-		
+	private int E;
+	private int V;
+	private Bag<DirectedEdge>[] adj;
+	
+	public EdgeWeightedDigraph(int V) {
+		this.V = V;
+		this.E = 0;
+		adj = (Bag<DirectedEdge>[]) new Bag[V];
+		for (int i = 0; i < V; i++) {
+			adj[i] = new Bag<DirectedEdge>();
+		}
+	}
+	
+	public EdgeWeightedDigraph(In in) {
+		this(in.readInt());
+		int edge = in.readInt();
+		try {
+			for (int i = 0; i < edge; i++) {
+				if (in.hasNextLine()) {
+					int v = in.readInt();
+					int w = in.readInt();
+					double weight = in.readDouble();
+					DirectedEdge e = new DirectedEdge(v, w, weight);
+					addEdge(e);
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void addEdge(DirectedEdge e) {
+		int v = e.from();
+		adj[v].add(e);
+		E++;
+	}
+	
+	public int V(){
+		return V;
+	}
+	
+	public int E(){
+		return E;
+	}
+	
+	public Iterable<DirectedEdge> adj(int v){
+		return adj[v];
+	}
+	
+	public String toString() {
+		String s = V + " vertices, " + E + " edges\n";
+		for (int v = 0; v < V; v++) {
+			s += v + ": ";
+			for (DirectedEdge e : adj[v]) {
+				s += e + " ";
+			}
+			s += "\n";
+		}
+		return s;
 	}
 }
