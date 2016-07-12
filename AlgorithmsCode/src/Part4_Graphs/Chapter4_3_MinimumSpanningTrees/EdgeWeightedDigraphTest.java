d9478f6c011e6e94967f82bd79bf8de717f51eed
detective0922 <detective0922@hotmail.com>
2016-07-12 10:09:14 +0800

    add test

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_3_MinimumSpanningTrees/EdgeWeightedDigraphTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_3_MinimumSpanningTrees/EdgeWeightedDigraphTest.java
@@ -1,12 +1,18 @@
 package Part4_Graphs.Chapter4_3_MinimumSpanningTrees;
 
+import java.io.File;
+
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
 
 public class EdgeWeightedDigraphTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		File tFile = new File("algs4-data//tinyEWG.txt");		
+		In in = new In(tFile);
+		EdgeWeightedGraph g = new EdgeWeightedGraph(in);
+		System.out.println(g);
 	}
 
 }
