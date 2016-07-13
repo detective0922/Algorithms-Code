873200b5561ac3d202fa707dbcfd463a4c48e1f9
detective0922 <detective0922@hotmail.com>
2016-07-13 14:24:30 +0800

    alp

---
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_4_ShortestPaths/AcyclicLPTest.java
@@ -0,0 +1,83 @@
+package Part4_Graphs.Chapter4_4_ShortestPaths;
+
+import java.io.File;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Topological;
+import edu.princeton.cs.algs4.EdgeWeightedDigraph;
+import edu.princeton.cs.algs4.DirectedEdge;
+
+public class AcyclicLPTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File tFile = new File("algs4-data//tinyEWDAG.txt");		
+		In in = new In(tFile);
+		EdgeWeightedDigraph g = new EdgeWeightedDigraph(in);
+		
+		int s = 5;
+		AcyclicLP sp = new AcyclicLP(g, s);
+		//edu.princeton.cs.algs4.AcyclicLP sp = new edu.princeton.cs.algs4.AcyclicLP(g, s);
+		for (int v = 0; v < g.V(); v++) {
+			StdOut.print(s + " to " + v);
+			StdOut.printf(" (%4.2f): ", sp.distTo(v));
+			if (sp.hasPathTo(v)) {
+				for (DirectedEdge e : sp.pathTo(v)) {
+					StdOut.print(e + " ");
+				}
+			}
+			StdOut.println();
+		}
+
+	}
+
+}
+
+class AcyclicLP{
+	private double[] distTo;
+    private DirectedEdge[] edgeTo;
+	
+	public AcyclicLP(EdgeWeightedDigraph g, int source) {
+		distTo = new double[g.V()];
+		edgeTo = new DirectedEdge[g.V()];
+		for (int v = 0; v < g.V(); v++) {
+			distTo[v] = Double.NEGATIVE_INFINITY;
+		}
+		distTo[source] = 0.0;
+		
+		Topological topological = new Topological(g);
+		for(int v: topological.order()){
+			relax(g, v);
+		}
+	}
+	
+	private void relax(EdgeWeightedDigraph g, int v) {
+		for (DirectedEdge e : g.adj(v)) {
+			int w = e.to();
+			if (distTo[w] < distTo[v] + e.weight()) {
+				distTo[w] = distTo[v] + e.weight();
+				edgeTo[w] = e;
+			}
+		}
+	}
+	
+	public boolean hasPathTo(int v) {
+		return distTo[v] < Double.POSITIVE_INFINITY;
+	}
+	
+	public double distTo(int v) {
+		return distTo[v];
+	}
+	
+	public Iterable<DirectedEdge> pathTo(int v) {
+		Stack<DirectedEdge> stack = new Stack<DirectedEdge>();
+		if (hasPathTo(v)) {
+			for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+				stack.push(e);
+			}
+		}
+		return stack;
+	}
+}
