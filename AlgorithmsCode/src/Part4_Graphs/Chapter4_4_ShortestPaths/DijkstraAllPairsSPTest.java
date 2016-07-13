96940e2725e689c1105979fd4857a03d47cb98c4
detective0922 <detective0922@hotmail.com>
2016-07-13 11:00:44 +0800

    dapsp

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_4_ShortestPaths/DijkstraAllPairsSPTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_4_ShortestPaths/DijkstraAllPairsSPTest.java
@@ -15,7 +15,17 @@ class DijkstraAllPairsSP{
 	
 	public DijkstraAllPairsSP(EdgeWeightedDigraph g) {
 		all = new DijkstraSP[g.V()];
-		
+		for(int v = 0;v<g.V();v++){
+			all[v] = new DijkstraSP(g, v);
+		}
 
+	}
+	
+	public Iterable<DirectedEdge> path(int s, int t) {
+		return all[s].pathTo(t);		
+	}
+	
+	public double dist(int s, int t) {
+		return all[s].distTo(t);
 	}
 }
