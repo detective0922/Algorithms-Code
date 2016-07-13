3a8ddad7b2525be1be6d207fd0bc0ae308ab5417
detective0922 <detective0922@hotmail.com>
2016-07-13 11:13:05 +0800

    asp

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_4_ShortestPaths/AcyclicSPTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_4_ShortestPaths/AcyclicSPTest.java
@@ -1,5 +1,7 @@
 package Part4_Graphs.Chapter4_4_ShortestPaths;
 
+import edu.princeton.cs.algs4.Stack;
+
 public class AcyclicSPTest {
 
 	public static void main(String[] args) {
@@ -10,5 +12,67 @@ public class AcyclicSPTest {
 }
 
 class AcyclicSP{
+	private double[] distTo;
+    private DirectedEdge[] edgeTo;
+    private boolean[] marked;
 	
+	public SP(EdgeWeightedDigraph g, int source) {
+		distTo = new double[g.V()];
+		edgeTo = new DirectedEdge[g.V()];
+		marked = new boolean[g.V()];
+		for (int v = 0; v < g.V(); v++) {
+			distTo[v] = Double.POSITIVE_INFINITY;
+		}
+		distTo[source] = 0.0;
+		
+		ShortPath(g, source);	
+		
+	}
+	
+	private void ShortPath(EdgeWeightedDigraph g, int source) {
+		relax(g, source);
+		marked[source] = true;
+		for (DirectedEdge e : g.adj(source)) {
+			int w = e.to();
+			if (!marked[w]) {
+				ShortPath(g, w);
+			}
+		}
+	}
+	
+	private void relax(DirectedEdge e) {
+		int v = e.from(), w = e.to();
+		if (distTo[w] > distTo[v] + e.getWeight()) {
+			distTo[w] = distTo[v] + e.getWeight();
+			edgeTo[w] = e;
+		}
+	}
+	
+	private void relax(EdgeWeightedDigraph g, int v) {
+		for (DirectedEdge e : g.adj(v)) {
+			int w = e.to();
+			if (distTo[w] > distTo[v] + e.getWeight()) {
+				distTo[w] = distTo[v] + e.getWeight();
+				edgeTo[w] = e;
+			}
+		}
+	}
+	
+	public boolean hasPathTo(int v) {
+		return distTo[v] < Double.POSITIVE_INFINITY;
+	}
+	
+	public double distTo(int v) {
+		return distTo[v];
+	}
+	
+	public Iterable<DirectedEdge> pathTo(int v) {
+		Stack<DirectedEdge> stack = new Stack<DirectedEdge>();
+		if (hasPathTo(v)) {
+			for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+				stack.push(e);
+			}
+		}
+		return stack;
+	}
 }
