53fdbc9644604d760f64d032339ba3b14750dda2
detective0922 <detective0922@hotmail.com>
2016-07-13 10:57:01 +0800

    dapsp

---
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_4_ShortestPaths/DijkstraAllPairsSPTest.java
@@ -0,0 +1,21 @@
+package Part4_Graphs.Chapter4_4_ShortestPaths;
+
+public class DijkstraAllPairsSPTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+class DijkstraAllPairsSP{
+	private DijkstraSP[] all;
+	
+	public DijkstraAllPairsSP(EdgeWeightedDigraph g) {
+		all = new DijkstraSP[g.V()];
+		
+
+	}
+}
