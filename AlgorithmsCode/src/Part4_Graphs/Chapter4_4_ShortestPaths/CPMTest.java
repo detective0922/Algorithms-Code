da8bbcae0abcdda70072733aae162b7f030938cd
detective0922 <detective0922@hotmail.com>
2016-07-19 14:40:54 +0800

    CPM

--- AlgorithmsCode/src/Part4_Graphs/Chapter4_4_ShortestPaths/CPMTest.java
+++ AlgorithmsCode/src/Part4_Graphs/Chapter4_4_ShortestPaths/CPMTest.java
@@ -24,7 +24,7 @@ public class CPMTest {
 		}
 		
 		int N = StdIn.readInt();
-		//String aa = StdIn.readLine();
+		String aa = StdIn.readLine();
 
         // source and sink
         int s = 2*N, t = 2*N + 1;
@@ -53,6 +53,41 @@ public class CPMTest {
             StdOut.printf("%4d %5.1f\n", i, lp.distTo(i));
         }
         StdOut.printf("Finish time: %5.1f\n", lp.distTo(t));
+		
+		// number of jobs
+        /*int N = StdIn.readInt();
+
+        // source and sink
+        int source = 2*N;
+        int sink   = 2*N + 1;
+
+        // build network
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(2*N + 2);
+        for (int i = 0; i < N; i++) {
+            double duration = StdIn.readDouble();
+            G.addEdge(new DirectedEdge(source, i, 0.0));
+            G.addEdge(new DirectedEdge(i+N, sink, 0.0));
+            G.addEdge(new DirectedEdge(i, i+N,    duration));
+
+            // precedence constraints
+            int M = StdIn.readInt();
+            for (int j = 0; j < M; j++) {
+                int precedent = StdIn.readInt();
+                G.addEdge(new DirectedEdge(N+i, precedent, 0.0));
+            }
+        }
+
+        StdOut.println(G);
+        // compute longest path
+        AcyclicLP lp = new AcyclicLP(G, source);
+
+        // print results
+        StdOut.println(" job   start  finish");
+        StdOut.println("--------------------");
+        for (int i = 0; i < N; i++) {
+            StdOut.printf("%4d %7.1f %7.1f\n", i, lp.distTo(i), lp.distTo(i+N));
+        }
+        StdOut.printf("Finish time: %7.1f\n", lp.distTo(sink));*/
 
 	}
 
