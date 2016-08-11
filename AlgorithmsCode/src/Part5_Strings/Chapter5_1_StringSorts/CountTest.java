22f483620f6e6cbcae59f4f9fcff791a7f237896
detective0922 <detective0922@hotmail.com>
2016-08-11 15:03:17 +0800

    count

--- AlgorithmsCode/src/Part5_Strings/Chapter5_1_StringSorts/CountTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_1_StringSorts/CountTest.java
@@ -1,16 +1,50 @@
 package Part5_Strings.Chapter5_1_StringSorts;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class CountTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		File tFile = new File("algs4-data//abra.txt");
 
+		try {
+			System.setIn(new FileInputStream(tFile.getAbsolutePath()));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String s = StdIn.readAll();
+		Alphabet alpha = new Alphabet(s);
+		int R = alpha.R();
+		int[] count = new int[R];
+		
+		String test = "ABCDR";
+		int N = test.length();
+		for (int i = 0; i < N; i++) {
+			char c = test.charAt(i);
+			if (alpha.contains(c)) {
+				count[alpha.toIndex(c)]++;
+			}
+		}
+
+		for (int j = 0; j < R; j++) {
+			StdOut.println(alpha.toChar(j) + " " + count[j]);
+		}
+		
 	}
 
 }
 
 class Alphabet{
-	public Alphabet(){
+	public Alphabet(String s){
 		
 	}
 	
