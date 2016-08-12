e9b0474bdfd7246260434da62fd7f7b7f5450867
detective0922 <detective0922@hotmail.com>
2016-08-12 14:01:32 +0800

    MSD

--- AlgorithmsCode/src/Part5_Strings/Chapter5_1_StringSorts/MSDTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_1_StringSorts/MSDTest.java
@@ -1,8 +1,30 @@
 package Part5_Strings.Chapter5_1_StringSorts;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class MSDTest {
 	public static void main(String[] args){
+		File tFile = new File("algs4-data//shells.txt");
+
+		try {
+			System.setIn(new FileInputStream(tFile.getAbsolutePath()));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		String[] test = StdIn.readAllStrings();
+		
+		MSD.sort(test);
+
+		for (int i = 0; i < test.length; i++) {
+			StdOut.println(test[i]);
+		}
 	}
 
 }
