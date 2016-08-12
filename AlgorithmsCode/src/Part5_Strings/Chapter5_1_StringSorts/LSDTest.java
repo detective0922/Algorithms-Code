75a9997333680b0430a128f66be6bd3e06f7ee32
detective0922 <detective0922@hotmail.com>
2016-08-12 10:53:56 +0800

    LSD

--- AlgorithmsCode/src/Part5_Strings/Chapter5_1_StringSorts/LSDTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_1_StringSorts/LSDTest.java
@@ -1,24 +1,45 @@
 package Part5_Strings.Chapter5_1_StringSorts;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class LSDTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		File tFile = new File("algs4-data//words3.txt");
 
+		try {
+			System.setIn(new FileInputStream(tFile.getAbsolutePath()));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String[] test = StdIn.readAllStrings();
+		
+		int w = test[0].length();
+		LSD.sort(test, w);
+
+		for (int i = 0; i < test.length; i++) {
+			StdOut.println(test[i]);
+		}
+		
 	}
 
 }
 
-class LSD{
-	public LSD(){
-		
+class LSD {
+
+	public static void sort(int[] a) {
+
 	}
-	
-	public void sort(int[] a){
-		
-	}
-	
-	public void sort(String[] a, int W) {
-		
+
+	public static void sort(String[] a, int W) {
+
 	}
 }
