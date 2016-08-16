8e2068e5735e8ff9f0334bac3e2ae5ea2edf15d7
detective0922 <detective0922@hotmail.com>
2016-08-16 10:23:38 +0800

    q3

--- AlgorithmsCode/src/Part5_Strings/Chapter5_1_StringSorts/Quick3StringTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_1_StringSorts/Quick3StringTest.java
@@ -1,11 +1,32 @@
 package Part5_Strings.Chapter5_1_StringSorts;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class Quick3StringTest {
 	
 	public static void main(String[] args){
+		File tFile = new File("algs4-data//shells.txt");
+
+		try {
+			System.setIn(new FileInputStream(tFile.getAbsolutePath()));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		String[] test = StdIn.readAllStrings();
+		
+		Quick3String.sort(test);
+
+		for (int i = 0; i < test.length; i++) {
+			StdOut.println(test[i]);
+		}
 	}
 
 }
