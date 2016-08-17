34f685b2d7bb3d85a04a8f3558682248597e9e4e
detective0922 <detective0922@hotmail.com>
2016-08-17 14:51:26 +0800

    tst

---
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_2_Tries/TriesSTTest.java
@@ -0,0 +1,16 @@
+package Part5_Strings.Chapter5_2_Tries;
+
+public class TriesSTTest {
+
+}
+
+class TriesST{
+	
+	private int R = 256;
+	
+	private class Node{
+		private Object val;
+		private Node[] next = new Node[R];
+	}
+	
+}
