cdf9b938a7c488a62905ff977763298f0a63396c
detective0922 <detective0922@hotmail.com>
2016-08-17 15:25:15 +0800

    tst

--- AlgorithmsCode/src/Part5_Strings/Chapter5_2_Tries/TriesSTTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_2_Tries/TriesSTTest.java
@@ -7,10 +7,33 @@ public class TriesSTTest {
 class TriesST<Value>{
 	
 	private static int R = 256;
+	private Node root;
 
 	private static class Node {
 		private Object val;
 		private Node[] next = new Node[R];
+	}
+	
+	public Value get(String key){
+		int d = 0;
+		int len = key.length();
+		Node x = root;
+		while (d <= len) {
+			if (x == null) {
+				return null;
+			}
+			if (d == len) {
+				return (Value) x.val;
+			}
+			char c = key.charAt(d);
+			x = x.next[c];
+			d++;
+		}
+		return null;
+	}
+	
+	public void put(String key, Value val){
+		
 	}
 	
 	
