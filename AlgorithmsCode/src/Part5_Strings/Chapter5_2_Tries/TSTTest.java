0ab46e2093b402d2611c698dfb92a9e383581442
detective0922 <detective0922@hotmail.com>
2016-10-11 02:03:18 +0800

    tst

--- AlgorithmsCode/src/Part5_Strings/Chapter5_2_Tries/TSTTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_2_Tries/TSTTest.java
@@ -333,7 +333,6 @@ class TST<Value> {
 	public String longestPrefixOf(String s) {
 		/*
 		
-		
 		Queue<String> tmpKeys = new Queue<String>();
 		Queue<Node> q = new Queue<Node>();
 		q.enqueue(x.mid);
