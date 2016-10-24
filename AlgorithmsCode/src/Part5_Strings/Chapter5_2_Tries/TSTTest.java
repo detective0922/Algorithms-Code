57cd91899274c203756acdb8771c2c1fb7ede14b
detective0922 <detective0922@hotmail.com>
2016-10-25 02:52:52 +0800

    tst

--- AlgorithmsCode/src/Part5_Strings/Chapter5_2_Tries/TSTTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_2_Tries/TSTTest.java
@@ -205,7 +205,7 @@ class TST<Value> {
         char c = key.charAt(d);
         if      (c < x.c)              return get(x.left,  key, d);
         else if (c > x.c)              return get(x.right, key, d);
-        else if (d < key.length() - 1) return get(x.mid,   key, d+1);
+        
        
     }*/
 	
