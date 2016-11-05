32cb4438ec529aff264d8ed04bbd3a5c15c622c3
detective0922 <detective0922@hotmail.com>
2016-11-05 22:26:38 +0800

    tst

--- AlgorithmsCode/src/Part5_Strings/Chapter5_2_Tries/TSTTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_2_Tries/TSTTest.java
@@ -200,7 +200,7 @@ class TST<Value> {
     
     
        
-       have length >= 1");
+       length >= 1");
         if (x == null) return null;
         char c = key.charAt(d);
         if      (c < x.c)              return get(x.left,  key, d);
