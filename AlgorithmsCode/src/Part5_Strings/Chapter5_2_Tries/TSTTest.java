a2f18d7a34926cb6eef48734bc5799a5e100cac5
detective0922 <detective0922@hotmail.com>
2016-11-18 15:26:45 +0800

    tst

--- AlgorithmsCode/src/Part5_Strings/Chapter5_2_Tries/TSTTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_2_Tries/TSTTest.java
@@ -207,7 +207,36 @@ class TST<Value> {
        
     }*/
 	
-	public Iterable<String> keysWithPrefix(String prefix){	
+	public Iterable<String> keysWithPrefix(String prefix) {
+        Queue<String> queue = new Queue<String>();
+        Node x = get(root, prefix, 0);
+        if (x == null) return queue;
+        if (x.val != null) queue.enqueue(prefix);
+        collect(x.mid, new StringBuilder(prefix), queue);
+        return queue;
+    }
+	
+	private Node get(Node x, String key, int d) {
+        if (key == null) throw new NullPointerException();
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        if (x == null) return null;
+        char c = key.charAt(d);
+        if      (c < x.c)              return get(x.left,  key, d);
+        else if (c > x.c)              return get(x.right, key, d);
+        else if (d < key.length() - 1) return get(x.mid,   key, d+1);
+        else                           return x;
+    }
+
+    private void collect(Node x, StringBuilder prefix, Queue<String> queue) {
+        if (x == null) return;
+        collect(x.left,  prefix, queue);
+        if (x.val != null) queue.enqueue(prefix.toString() + x.c);
+        collect(x.mid,   prefix.append(x.c), queue);
+        prefix.deleteCharAt(prefix.length() - 1);
+        collect(x.right, prefix, queue);
+    }
+	
+	/*public Iterable<String> keysWithPrefix(String prefix){	
 		Queue<String> keys = new Queue<String>();
 		int prefixLen = prefix.length();
 		Node x = root;
@@ -257,7 +286,7 @@ class TST<Value> {
 			}
 		}
 		return keys;
-	}
+	}*/
 	
 	public boolean contains(String key){
 		return get(key) != null;
