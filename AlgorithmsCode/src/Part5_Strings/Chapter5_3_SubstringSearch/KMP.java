a77dc8d1be5205a72a25db63db633f9202fa0498
detective0922 <detective0922@hotmail.com>
2016-11-21 15:07:40 +0800

    kmp

--- AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/KMP.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/KMP.java
@@ -1,5 +1,30 @@
 package Part5_Strings.Chapter5_3_SubstringSearch;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class KMP {
+	
+	public static void main(String[] args) {
+		
+		String txt = "";
+		String pat = "";
+		KMP kmp = new KMP(pat);
+		int offset = kmp.search(txt);
+		StdOut.println("text:     " + txt);
+		StdOut.println("pattern:  " + txt);
+		for(int i = 0;i<offset;i++){
+			StdOut.print(" ");
+		}
+		StdOut.println(pat);	
+	}
+	
+	public KMP(String pat) {
+
+	}
+	
+	public int search(String txt){
+		
+	}
 
 }
+
