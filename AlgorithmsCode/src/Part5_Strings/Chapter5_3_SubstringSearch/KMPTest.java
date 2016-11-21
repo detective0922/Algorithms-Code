a47435610c3e5d3d2a73b63ad771b1a94c96b31b
detective0922 <detective0922@hotmail.com>
2016-11-21 15:32:25 +0800

    kmp

--- AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/KMPTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/KMPTest.java
@@ -1,6 +1,7 @@
 package Part5_Strings.Chapter5_3_SubstringSearch;
 
 import edu.princeton.cs.algs4.StdOut;
+import sun.tools.jar.resources.jar;
 
 public class KMPTest {
 	
@@ -23,12 +24,41 @@ public class KMPTest {
 
 class KMP{
 	
+	private String pat;
+	private int[][] dfa;
+	
 	public KMP(String pat) {
-
+		this.pat = pat;
+		int R = 256;
+		int M = pat.length();
+		
+		dfa = new int[R][M];
+		dfa[pat.charAt(0)][0] = 1;
+		int X = 0;
+		for (int j = 1; j < M; j++) {
+			for (char c = 0; c < R; c++) {
+				dfa[c][j] = dfa[X][j];
+			}
+			dfa[pat.charAt(j)][j] = j + 1;
+			X = dfa[pat.charAt(j)][X];
+		}
+		
 	}
 	
 	public int search(String txt){
+		int M = pat.length();
+		int N = txt.length();
+		int j = 0;
+		int i = 0;
+		for (; i < N & j < M; i++) {
+			j = dfa[pat.charAt(i)][j];
+		}
 		
+		if (j == M) {
+			return i-M;
+		} else {
+
+		}
 	}
 
 }
