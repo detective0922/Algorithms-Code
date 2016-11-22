14b1d7aa080a9278e371c58d865024c8d64d86ae
detective0922 <detective0922@hotmail.com>
2016-11-22 14:49:35 +0800

    rk

--- AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/RKTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/RKTest.java
@@ -1,10 +1,26 @@
 package Part5_Strings.Chapter5_3_SubstringSearch;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class RKTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String txt = "AABRAACADABRAACAADABRA";
+		String pat = "AACAA";
+		RabinKarp rk = new RabinKarp(pat);
+		int offset = rk.search(txt);
+		StdOut.println("text:     " + txt);
+		StdOut.print("pattern:  ");
+		for(int i = 0;i<offset;i++){
+			StdOut.print(" ");
+		}
+		StdOut.println(pat);	
+		StdOut.println(offset);
 
 	}
 
+}
+
+class RabinKarp{
+	
 }
