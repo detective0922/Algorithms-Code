93fb2af8bbc20a3b1d5bf3dcdaca5724d6fcc31e
detective0922 <detective0922@hotmail.com>
2016-11-22 13:36:05 +0800

    bm

--- AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/BMTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/BMTest.java
@@ -1,6 +1,21 @@
 package Part5_Strings.Chapter5_3_SubstringSearch;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class BMTest {
+	public static void main(String[] args) {
+		String txt = "AABRAACADABRAACAADABRA";
+		String pat = "AACAA";
+		KMP kmp = new KMP(pat);
+		int offset = kmp.search(txt);
+		StdOut.println("text:     " + txt);
+		StdOut.print("pattern:  ");
+		for (int i = 0; i < offset; i++) {
+			StdOut.print(" ");
+		}
+		StdOut.println(pat);
+		StdOut.println(offset);
+	}
 
 }
 
