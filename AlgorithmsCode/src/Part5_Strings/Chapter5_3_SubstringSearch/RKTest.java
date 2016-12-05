55a96141e2a2b8aad360fabd0f24c1981f5a2f41
detective0922 <detective0922@hotmail.com>
2016-12-05 16:15:31 +0800

    rk

--- AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/RKTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/RKTest.java
@@ -1,5 +1,7 @@
 package Part5_Strings.Chapter5_3_SubstringSearch;
 
+import java.util.Iterator;
+
 import edu.princeton.cs.algs4.StdOut;
 
 public class RKTest {
@@ -22,5 +24,24 @@ public class RKTest {
 }
 
 class RabinKarp{
+	private String pat;
+	private long patHash;
+	private int R = 256;
+	private long Q;
+	
+	public RabinKarp(String pat){
+		this.pat = pat;
+		
+		
+	}
+	
+	private long hash(String key, int M) {
+		long h = 0;
+		for (int i = 0; i < M; i++) {
+			h = (R * h + key.charAt(i)) % Q;
+		}
+		return h;
+	}
+	
 	
 }
