e54744685586199c0dd7a82e4dfca222b0646dc6
detective0922 <detective0922@hotmail.com>
2016-12-05 16:50:29 +0800

    rk

--- AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/RKTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_3_SubstringSearch/RKTest.java
@@ -48,8 +48,15 @@ class RabinKarp{
 		return h;
 	}
 	
+	private boolean check(String txt, int i) {
+        for (int j = 0; j < M; j++) 
+            if (pat.charAt(j) != txt.charAt(i + j)) 
+                return false; 
+        return true;
+    }
+	
 	public int search(String txt) {
-
+		
 	}
 	
 	
