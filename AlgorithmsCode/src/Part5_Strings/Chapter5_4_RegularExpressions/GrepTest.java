a0704de293ed3d7d5aa74de456f97bd954777e96
detective0922 <detective0922@hotmail.com>
2016-12-06 14:10:57 +0800

    nfa

--- AlgorithmsCode/src/Part5_Strings/Chapter5_4_RegularExpressions/GrepTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_4_RegularExpressions/GrepTest.java
@@ -1,9 +1,17 @@
 package Part5_Strings.Chapter5_4_RegularExpressions;
 
+import java.io.File;
+
+import edu.princeton.cs.algs4.In;
+
 public class GrepTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String regexp = "(A*B|AC)D";
+		File tFile = new File("algs4-data//tinyL.txt");		
+		In in = new In(tFile);
+		
 
 	}
 
