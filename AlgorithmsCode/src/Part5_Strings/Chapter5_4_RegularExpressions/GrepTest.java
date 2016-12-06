2fcfc21cba8c95a9578e41073b63d18d085207cb
detective0922 <detective0922@hotmail.com>
2016-12-06 14:16:19 +0800

    nfa

--- AlgorithmsCode/src/Part5_Strings/Chapter5_4_RegularExpressions/GrepTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_4_RegularExpressions/GrepTest.java
@@ -1,8 +1,12 @@
 package Part5_Strings.Chapter5_4_RegularExpressions;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.NFA;
+import edu.princeton.cs.algs4.StdOut;
 
 public class GrepTest {
 
@@ -11,7 +15,19 @@ public class GrepTest {
 		String regexp = "(A*B|AC)D";
 		File tFile = new File("algs4-data//tinyL.txt");		
 		In in = new In(tFile);
-		
+		try {
+			System.setIn(new FileInputStream(tFile.getAbsolutePath()));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		NFA nfa = new NFA(regexp);
+		while(in.hasNextLine()){
+			String txt = in.readLine();
+			if(nfa.recognizes(txt)){
+				StdOut.println(txt);
+			}
+		}
 
 	}
 
