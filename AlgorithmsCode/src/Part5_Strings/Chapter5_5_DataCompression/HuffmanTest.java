6d10471e837d62f473e17b72e9ff9c78360c0649
detective0922 <detective0922@hotmail.com>
2016-12-07 11:22:15 +0800

    huffman

--- AlgorithmsCode/src/Part5_Strings/Chapter5_5_DataCompression/HuffmanTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_5_DataCompression/HuffmanTest.java
@@ -4,7 +4,15 @@ public class HuffmanTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		if      (args[0].equals("-")) compress();
+        else if (args[0].equals("+")) expand();
 
 	}
 
+}
+
+class Huffman {
+	private static class Node implements Comparable<Node> {
+		
+	}
 }
