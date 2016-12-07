bada38714ca7ee0177baf8609ac9936b84995716
detective0922 <detective0922@hotmail.com>
2016-12-07 11:24:27 +0800

    huffman

--- AlgorithmsCode/src/Part5_Strings/Chapter5_5_DataCompression/HuffmanTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_5_DataCompression/HuffmanTest.java
@@ -14,5 +14,15 @@ public class HuffmanTest {
 class Huffman {
 	private static class Node implements Comparable<Node> {
 		
+		private char ch;
+		private int freq;
+		private final Node left, right;
+
+		@Override
+		public int compareTo(Node o) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
 	}
 }
