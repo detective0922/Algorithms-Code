728ab7f6e8ef42e4b22e4411f1764e2b4fe51c91
detective0922 <detective0922@hotmail.com>
2016-12-16 13:54:53 +0800

    lzw

--- AlgorithmsCode/src/Part5_Strings/Chapter5_5_DataCompression/LZWTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_5_DataCompression/LZWTest.java
@@ -11,6 +11,24 @@ public class LZWTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		File tFile = new File("algs4-data//abra.txt");
+		File binFile = new File("bin.txt");
+		PrintStream standardOut = System.out;
+		
+		try {
+			System.setIn(new FileInputStream(tFile.getAbsolutePath()));
+			PrintStream ps = new PrintStream(new FileOutputStream(binFile));
+			System.setOut(ps);
+			//edu.princeton.cs.algs4.Huffman.compress();
+			Huffman.compress();
+			System.setOut(standardOut);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		
+		Huffman.BinaryDump(binFile);
 		
 	}
 
