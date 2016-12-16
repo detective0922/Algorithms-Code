ab0c706d131179c7ba1eb3899999a78ae43e1c1b
detective0922 <detective0922@hotmail.com>
2016-12-16 13:56:35 +0800

    lzw

--- AlgorithmsCode/src/Part5_Strings/Chapter5_5_DataCompression/LZWTest.java
+++ AlgorithmsCode/src/Part5_Strings/Chapter5_5_DataCompression/LZWTest.java
@@ -35,6 +35,13 @@ public class LZWTest {
 }
 
 class LZW{
+	private static final int R = 256;
+	private static final int L = 4096;
+	private static final int W = 12;
+	
+	public static void compress() {
+
+	}
 	
 }
 
